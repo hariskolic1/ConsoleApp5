dac783648d77cdc50136e0721fe495132c9f8a93
PC <PC@DESKTOP-ARTMFB0>
2021-08-20 18:22:37 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            List<string> Imenik = new();
+           
+
+
+            do
+            {
+                Console.WriteLine("Unesite novo ime");
+                string unos = Console.ReadLine();
+                if (unos == "q")
+                {
+                    break;
+                }
+            } while (true);
+            foreach (string ime in Imenik)
+            {
+                Console.WriteLine(ime);
+            }
+
+            
+        }
+    }
+}
